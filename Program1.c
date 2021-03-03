bcd494a39cc33e6472721619e073f73f6929bfc3
ThanosCOVID <danielburns225@protonmail.com>
2021-03-03 01:01:37 -0500

    Update Program1.c

--- Program1.c
+++ Program1.c
@@ -2,11 +2,26 @@
 #include <pthread.h>
 #include "programs.h"
 
+//students are semaphores
 void Student(int id) {
-    //pthread_t studentThread = malloc(sizeof(studentThread));
+  int i;
+  int length = (id % 4) + 1;
+  
+  sem_t studentSem;
+  //pthread_t studentThread = malloc(sizeof(studentThread));
+  EnterOffice(id);
+
+  for(i = 0; i < length; i++) {
+    QuestionStart(id);
+    QuestionDone(id);
+  }
+
 }
 
+//professor is a thread
 void Professor() {
+  pthread_t professorThread = malloc(sizeof(professorThread));
+
   int i;
 	for(i = 0; i < 2; i++) {
 		AnswerStart(i);
